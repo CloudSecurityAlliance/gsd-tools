a81a8a7da8c7996141f419f3c4ff1c7e44204b9c
Josh Buker <crypto@joshbuker.com>
2022-05-31 16:39:17 -0700

    Create placeholder for create GSD form dialog

--- gsd-web/src/components/CreateDialog.vue
+++ gsd-web/src/components/CreateDialog.vue
@@ -1,25 +1,18 @@
 <template>
-  <!-- notice dialogRef here -->
-  <q-dialog ref="dialogRef" @hide="onDialogHide" :persistent="unsavedChanges">
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
     <q-card class="q-dialog-plugin">
       <q-card-section class="bg-primary text-white text-center">
-        <div class="text-h6">Edit GSD</div>
+        <div class="text-h6">Create new GSD Identifier</div>
       </q-card-section>
 
       <q-separator />
 
       <q-card-section style="overflow: auto; max-height: 80vh;">
-        <q-input
-          v-model="gsdJson"
-          filled
-          autogrow
-          label="GSD JSON"
-        />
+        <p>Hello world</p>
       </q-card-section>
 
       <q-card-actions align="right">
         <q-btn flat label="Cancel" color="grey" v-close-popup />
-        <q-btn flat label="Save Changes" color="primary" @click="saveChanges" :disabled="!unsavedChanges" />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -33,18 +26,7 @@ import { api } from 'boot/axios'
 import { errorNotification } from '../misc/ErrorNotification'
 
 export default {
-  name: 'EditDialog',
-
-  props: {
-    gsd_json: {
-      type: String,
-      required: true
-    },
-    identifier: {
-      type: String,
-      required: true
-    }
-  },
+  name: 'CreateDialog',
 
   emits: [
     // REQUIRED; need to specify some events that your
@@ -64,67 +46,8 @@ export default {
 
     const $q = useQuasar()
 
-    const gsdJson = ref(props.gsd_json)
-
-    const unsavedChanges = computed(
-      () => {
-        return (props.gsd_json !== gsdJson.value)
-      }
-    )
-
-    // console.log(unsavedChanges.value)
-
-    // watch(
-    //   () => gsdJson.value,
-    //   (newValue) => {
-    //     console.log(unsavedChanges.value)
-    //   }
-    // )
-
-    function saveChanges() {
-      let fileContent = '';
-      try {
-        // Force reformatting of the JSON string, as well as check validity.
-        fileContent = JSON.stringify(JSON.parse(gsdJson.value), null, 2);
-        api.patch('/update-gsd', {
-          identifier: props.identifier,
-          file_content: fileContent
-        }).then(
-          (response) => {
-            const redirectWindow = window.open(
-              response.data.redirect_url,
-              '_blank'
-            )
-            if(!redirectWindow) {
-              $q.notify({
-                color: 'negative',
-                position: 'top',
-                message: 'Failed to open issue in a new tab',
-                icon: 'report_problem'
-              })
-            }
-            $q.notify({
-              color: 'positive',
-              position: 'top',
-              message: 'Changes saved!',
-              icon: 'published_with_changes'
-            })
-            onCancelClick()
-          },
-          (error) => {
-            errorNotification(error, 'Failed to update GSD')
-          }
-        )
-      } catch(error) {
-        errorNotification(error, 'Failed to save changes')
-      }
-    }
-
     return {
       // Custom stuff
-      gsdJson,
-      saveChanges,
-      unsavedChanges,
 
       // This is REQUIRED;
       // Need to inject these (from useDialogPluginComponent() call)
