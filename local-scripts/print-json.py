a1feb26feff9a90cc08cdbc442b64f1382d70d0c
Kurt Seifried <kurt@seifried.org>
2022-08-29 10:48:40 -0600

    Fixed indent

--- local-scripts/print-json.py
+++ local-scripts/print-json.py
@@ -2,6 +2,8 @@
 
 import sys
 import json
+import os
+import re
 
 # Take a file name to update
 
@@ -9,9 +11,20 @@ file = sys.argv[1]
 
 # open, read the file, generate the json.dumps with indent=2 and close it
 
+def set_file_indent(file):
+    # This code breaks in 2030 and.or after we assign 1 million GSDs per year
+    file_name = os.path.basename(file)
+    if re.match("GSD-202[1-9]-1[0-9][0-9][0-9][0-9][0-9][0-9]", file_name):
+        indent = 2
+    else:
+        indent = 4
+    return(indent)
+
+file_indent = set_file_indent(file)
+
 with open(file, "r") as f:
     data = json.load(f)
-    output = json.dumps(data, indent=2)
+    output = json.dumps(data, indent=file_indent)
     f.close()
 
 # open, write the file and close
