694ac60d02f8fcf35ee95b1bb398454ce2307894
Josh Buker <crypto@joshbuker.com>
2022-10-14 13:53:56 -0700

    Update dependencies

---
+++ gsd-web/src/pages/Redirect.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Cloud Security Alliance logo"
+      src="~assets/csa-logo.svg"
+      style="width: 75vw; max-width: 600px;"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageRedirect'
+})
+</script>
