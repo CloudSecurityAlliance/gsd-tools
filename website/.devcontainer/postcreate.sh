7dcc6aa2ed4331bbd5169e0579ba1ac961d26a1e
Josh Buker <crypto@joshbuker.com>
2023-03-05 10:31:58 -0800

    Demonstrate safe mode error

--- website/.devcontainer/postcreate.sh
+++ website/.devcontainer/postcreate.sh
@@ -2,4 +2,4 @@
 
 bundle config set path vendor/bundle
 bundle install --jobs=1
-bundle exec jekyll serve -w --host localhost --port 3000
+bundle exec jekyll serve -w --safe --host localhost --port 3000 --trace
